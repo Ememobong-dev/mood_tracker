8f86d8e0f0b544e930d84b8830a9580b8a60d97f
Olushola <ememobongumoren2@gmail.com>
2025-05-12 15:36:23 +0100

    update

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center bg-transparent fixed top-0 py-3 px-28'>
+        
+        
+    </div>
+  )
+}
