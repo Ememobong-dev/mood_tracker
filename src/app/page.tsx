eaccdeddba4bef52cefd6ee5671b91c7ff220e76
Olushola <ememobongumoren2@gmail.com>
2025-05-12 17:31:51 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import MoodSleepChart from "@/components/MoodTracker";
 import { Navbar } from "@/components/Navbar";
 import { ArrowRightOutlined } from "@ant-design/icons";
 import { Col, Row } from "antd";
+import { FaBed, FaMeh } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -28,7 +29,7 @@ export default function Home() {
       <div className="mt-16 px-28">
         <Row gutter={[32, 32]}>
           <Col xs={8}>
-            <div className="bg-white p-4 h-full rounded-lg border border-white/50 flex flex-col gap-5">
+            <div className="bg-white p-4 h-full rounded-2xl border border-gray-500/50 flex flex-col gap-5">
               <div className="flex flex-col gap-3">
                 <div className="flex gap-1 items-center">
                   <p className="text-2xl text-blue-950 font-semibold">
@@ -39,9 +40,15 @@ export default function Home() {
                   </p>
                 </div>
                 <div className="flex flex-col gap-3 justify-center h-[150px] p-4 rounded-2xl w-full bg-blue-300">
-                  <p className="text-blue-950 font-semibold text-2xl">
-                    Neutral
-                  </p>
+                  <div className="flex gap-2 items-center">
+                    <span>
+                      <FaMeh color="#f7f6f6" size={24} />
+                    </span>
+                    <p className="text-blue-950 font-semibold text-2xl">
+                      Neutral
+                    </p>
+                  </div>
+
                   <div className="flex gap-2 items-center">
                     <span>
                       <ArrowRightOutlined />
@@ -63,7 +70,14 @@ export default function Home() {
                   </p>
                 </div>
                 <div className="flex flex-col gap-3 justify-center h-[150px] p-4 rounded-2xl w-full bg-blue-800">
-                  <p className="text-white font-semibold text-2xl">5-6 Hours</p>
+                  <div className="flex gap-2 items-center">
+                    <span>
+                      <FaBed color="#f7f6f6" size={24} />
+                    </span>
+                    <p className="text-white font-semibold text-2xl">
+                      5-6 Hours
+                    </p>
+                  </div>
                   <div className="flex gap-2 items-center">
                     <span
                       style={{
@@ -71,7 +85,9 @@ export default function Home() {
                         display: "inline-block",
                       }}
                     >
-                      <ArrowRightOutlined style={{ color: "white", opacity: "50%" }} />
+                      <ArrowRightOutlined
+                        style={{ color: "white", opacity: "50%" }}
+                      />
                     </span>
                     <p className="text-white/50 font-medium">
                       Increase from the previous 5 check-ins
@@ -82,8 +98,8 @@ export default function Home() {
             </div>
           </Col>
           <Col xs={16}>
-            <div className="bg-white p-4 h-full rounded-lg border border-white/50">
-              <h3>Moods and sleep trends</h3>
+            <div className="bg-white p-4 h-full rounded-2xl border border-gray-500/50">
+              <h3 className="text-blue-950 text-3xl font-bold">Mood and sleep trends</h3>
               <MoodSleepChart />
             </div>
           </Col>
